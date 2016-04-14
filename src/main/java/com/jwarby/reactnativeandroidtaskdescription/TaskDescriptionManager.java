bf096ddf96754a11fd7a0a929aac25a2f5ab7384
James Warwood <james.duncan.1991@googlemail.com>
2016-04-14 11:43:02 +0800

    Add single private method for updating TaskDescription
    Run TaskDescription update on UI thread
    
    Only run the update for SDK versions >= 21; don't import TaskManager directly
    in case we're running on a version of the SDK that doesn't have it
    
    Closes #1, #2 and #4

--- src/main/java/com/jwarby/reactnativeandroidtaskdescription/TaskDescriptionManager.java
+++ src/main/java/com/jwarby/reactnativeandroidtaskdescription/TaskDescriptionManager.java
@@ -2,8 +2,8 @@ package com.jwarby.reactnativeandroidtaskdescription;
 
 import android.app.Activity;
 import android.app.ActivityManager;
-import android.app.ActivityManager.TaskDescription;
 import android.graphics.Color;
+import android.os.Build;
 import android.view.ViewStub;
 import android.util.Log;
 
@@ -24,13 +24,34 @@ public class TaskDescriptionManager extends SimpleViewManager<ViewStub> {
   private static String REACT_CLASS = "TaskDescriptionAndroid";
 
   private Activity mActivity = null;
-  private String mColor = null;
+  private Integer mColor = null;
   private String mLabel = null;
 
   public TaskDescriptionManager(Activity activity) {
     super();
 
     mActivity = activity;
+  }
+
+  private void updateTaskDescription() {
+    mActivity.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        if (Build.VERSION.SDK_INT >= 21) {
+          ActivityManager.TaskDescription taskDesc;
+
+          if (mColor != null) {
+            taskDesc = new ActivityManager.TaskDescription(mLabel, null, mColor);
+          } else if (mLabel != null) {
+            taskDesc = new ActivityManager.TaskDescription(mLabel);
+          } else {
+            return;
+          }
+
+          mActivity.setTaskDescription(taskDesc);
+        }
+      }
+    });
   }
 
   @Override
@@ -45,27 +66,15 @@ public class TaskDescriptionManager extends SimpleViewManager<ViewStub> {
 
   @ReactProp(name = "backgroundColor")
   public void setBackgroundColor(ViewStub view, @Nullable String hex) {
-    mColor = hex;
+    mColor = Color.parseColor(hex);
 
-    final int color = Color.parseColor(hex);
-
-    final TaskDescription taskDesc = new TaskDescription(mLabel, null, color);
-
-    mActivity.setTaskDescription(taskDesc); 
+    updateTaskDescription();
   }
 
   @ReactProp(name = "label")
   public void setLabel(ViewStub view, @Nullable String text) {
-    TaskDescription taskDesc;
-
     mLabel = text;
 
-    if (mColor != null) {
-      taskDesc = new TaskDescription(text, null, Color.parseColor(mColor));
-    } else {
-      taskDesc = new TaskDescription(text);
-    }
-
-    mActivity.setTaskDescription(taskDesc);
+    updateTaskDescription();
   }
 }
